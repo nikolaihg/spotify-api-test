f8ce25659d08b0b18d48778e5410e369d463380b
nikolaihg <nikolai.gangstoe@gmail.com>
2024-11-11 17:44:34 +0100

    auth

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # spotify-api-test
 
-# 1. Prerequisites
+## Prequisites
 
 Spotify Developer Account: Sign up at the Spotify Developer Dashboard if you haven't already.
 Create a Spotify Application: In the Spotify Developer Dashboard, create a new app. This will provide you with the Client ID and Client Secret, which are needed for authentication.
--- main.py
+++ main.py
@@ -9,15 +9,12 @@ CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 REDIRECT_URL = os.getenv("SPOTIPY_REDIRECT_URI")
 
-# Set up Spotify client
-# For Client Credentials Flow:
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# For Authorization Code Flow:
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-#     client_id=client_id,
-#     client_secret=client_secret,
-#     redirect_uri=redirect_uri,
-#     scope="user-library-read"
-# ))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id="YOUR_CLIENT_ID",
+    client_secret="YOUR_CLIENT_SECRET",
+    redirect_uri="YOUR_REDIRECT_URI",
+    scope="user-library-read"
+))
